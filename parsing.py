ddeaf54d2ee9fd134992adb0ebfaccb9fe04aa6a
kirill-shi <shitkokirill@yandex.ru>
2023-03-22 20:39:54 +0300

    added my parsing program

--- parsing.py
+++ parsing.py
@@ -1,8 +1,28 @@
-import requests 
+#import requests 
 
-url = f'https://kidkodschool.github.io/welcome.html'
+#url = f'https://kidkodschool.github.io/welcome.html'
 
-response = requests.get(url)
+#response = requests.get(url)
 
-with open("./python_is_cool.html", 'wb') as f:
-    f.write(response.content)
+#with open("./python_is_cool.html", 'wb') as f:
+ #   f.write(response.content)
+
+import requests
+
+from bs4 import BeautifulSoup
+
+query = 'cats'
+
+url = f'https://www.kiddle.co/s.php?q={query}'
+
+page = requests.get(url).text #вернет объект
+soup = BeautifulSoup(page, 'html.parser')
+#print(soup)
+
+for raw_img in soup.find_all('img'): #находим все теги на странице
+    link = raw_img.get('src') #получаем значение из атриьута src
+    if link and link.startswith('https'): #если будет ссылка и она использует безопастный протокол 
+        response = requests.get(link) # получаем данные из этой ссылки 
+        with open("./today_avatar.jpg", 'wb') as f: #сохраняем результат в файл используя бинарную запись 
+            f.write(response.content)
+        break  
